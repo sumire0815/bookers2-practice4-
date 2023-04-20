5d97ca82f1cc4dec6652675b9633746d3685cb2a
sumire0815 <125476388+sumire0815@users.noreply.github.com>
2023-04-20 16:03:09 +0000

    [Finish]

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  
+
 devise_for :users, controllers: {   registrations: 'users/registrations',
                                     sessions: 'users/sessions' }
-  
+
   root :to =>"homes#top"
 
   get "home/about"=>"homes#about"
   resources :users
-  resources :books, only: [:new, :create, :index, :edit, :show, :destroy] do
+  resources :books, only: [:new, :create, :index, :show, :destroy,:edit,:update] do
     resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
   end
-  
+
 end
